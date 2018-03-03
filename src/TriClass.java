92ab4f2a545e0a62ce4995361bfc7ff1db64ae98
Tom Guerney <tguerney@gmail.com>
2018-03-03 18:26:22 +1100

    Add findLongestSide() to TriClass.java

--- src/TriClass.java
+++ src/TriClass.java
@@ -9,18 +9,25 @@
  * @author tguerney
  */
 public class TriClass {
-    
+
     public TriClass() {
-        
+
     }
-    
+
     public boolean isTriangle(int x, int y, int z) {
+        int longestSide = findLongestSide(x, y, z);
         return false;
     }
-    
+
+    private int findLongestSide(int x, int y, int z) {
+        int max = 0;
+        max = Math.max(x, y);
+        max = Math.max(max, z);
+        return max;
+    }
+
     public String triangleType(int x, int y, int z) {
         return "TODO";
     }
-    
-    
+
 }
